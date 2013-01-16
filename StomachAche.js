4ca570027df85f2aebf0bdb2d6c61d916c869d92
Sam <sam.von.ehren@gmail.com>
2013-01-16 14:07:53 -0500

    Target class is implemented along with a game over screen

--- StomachAche.js
+++ StomachAche.js
@@ -7,5 +7,14 @@ window.onload = (function() {
 	 Crafty.background("#000000");
 
 	Crafty.e("Player").attr({x: 500, y: 300, w: 50, h: 50});
-	Crafty.e("Ground").attr({x: 0, y: 500, w: 1000, h: 100});	
+	Crafty.e("Ground").attr({x: 0, y: 500, w: 1000, h: 100});
+	Crafty.e("Target").attr({x: 700, y: 450, w: 50, h: 50});
+
+	Crafty.scene("gameOver", function() {
+		var textHandler = Crafty.e("2D,DOM,Text,Keyboard").attr({x: 500, y:300}).text("Relief :)\nPress 'r' to restart").textColor("#FF0000");
+		textHandler.bind("KeyDown", function() {
+			if(this.isDown('R')) {
+				Crafty.scene("gameplay"); }
+			});
+		});
 });
--- StomachAcheComponents.js
+++ StomachAcheComponents.js
@@ -91,4 +91,53 @@ Crafty.c("Ground", {
 		}
 	}
 });
+
+Crafty.c("Target", {
+	displacement: 0,
+	side: true,
+	init: function() {
+		this.addComponent("2D,Canvas,Color, Collision");
+		this.color("#0000FF");
+		this.collision();
+		this.onHit("Player", function() {
+			Crafty.scene("gameOver");
+			});
+		this.bind("EnterFrame", this.coolDown);
+		this.bind("KeyDown", function() {
+			this.unbind("EnterFrame", this.coolDown);
+			this.bind("EnterFrame", this.incrementDisplacement);
+			});
+		this.bind("KeyUp", function() {
+			this.unbind("EnterFrame", this.incrementDisplacement);
+			this.bind("EnterFrame", this.coolDown);
+			});
+		},
+	incrementDisplacement: function() {
+		this.displacement += 0.1;
+		if(this.side){
+			this.x += this.displacement;
+			this.side = false;
+			}
+		else	{
+			this.x -= this.displacement;
+			this.side = true;
+			}
+	},
+	coolDown: function() {
+		this.displacement -= 0.05;
+		if(this.displacement < 0){
+			this.displacement = 0;
+			}
+		else {
+			if(this.side){
+				this.x += this.displacement;
+				this.side = false;
+				}
+			else	{
+				this.x -= this.displacement;
+				this.side = true;
+				}
+		}
+	}
+});
 	
